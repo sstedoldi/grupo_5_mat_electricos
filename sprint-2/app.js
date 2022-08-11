75d3e02e96a4d8ad94931d688c193099fd3c1c0a
Rubens2214 <rubens_722@hotmail.es>
2022-08-10 21:19:20 -0300

    prueba

--- sprint-2/app.js
+++ sprint-2/app.js
@@ -7,7 +7,7 @@ const path = require("path");
 app.use(express.static("public"));
 
 const port = 3000;
-app.listen(port, () => console.log("Servidor corriendo en puerto " + port));
+app.listen(port, () => console.log("Servidor corriendo en puerto ", 3000));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/home.html"));
