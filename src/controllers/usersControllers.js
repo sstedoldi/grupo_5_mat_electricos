9ae26fb1a679e822531d3ea2428cb3ad35ca6e8c
Rubens2214 <rubens_722@hotmail.es>
2022-09-12 00:04:31 -0300

    cree rutas controladores y jso de user, faltan estilos y terminar de darle el total de la funcionalidad

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,13 +1,86 @@
+////Primary modules
+const fs = require("fs");
+const path = require("path");
+
 //Data managing
+const usersFilePath = path.join(__dirname, "../data/users.json");
+const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
+
+//REGEX of thousand
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 //Controller definition
 const usersController = {
-  login: (req, res) => {
-    res.render("login");
+  index: (req, res) => {
+    res.render("users", {
+      users,
+      toThousand,
+  });
   },
-  register: (req, res) => {
+  loginUser: (req, res) => {
+    res.render("login.ejs");
+  },
+  register: (req, res)=>{
     res.render("register");
   },
+  detail: (req, res) => {
+    let idUser = req.params.idUser;
+    let user = users.find((oneUser) => oneUser.idUser == idUser);
+    res.render("userDetail", {
+      users,
+      toThousand,
+    });
+  },
+  registerUser: (req, res) => {
+    let newUser = {
+      idUser: users[users.length - 1].idUser + 1,
+      ...req.body,
+      usersImage: req.file ? req.file.filename : "default-image.png",      
+    };
+    users.push(newUser);
+    fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "));
+    res.redirect("/users/"); 
+  },
+
+  editUser: (req, res) => {
+    let idUser = req.params.idUser;
+    let userToEdit = users.find((oneUser) => oneUser.idUser == idUser);
+
+    res.render("/userEdit", {
+      userToEdit
+    });
+  },  
+  updateUser: (req, res) => {
+    let idUser = req.para
+    ms.idUser;
+    let userToEdit = users.find((oneUsers) => oneUsers.idUser == idUser);
+    userToEdit = {
+      idUser: userToEdit.idUser,
+      ...req.body,
+      imagen: userToEdit.imagen,
+    };
+
+    let newUser = users.map((users) => {
+      //nueva variable con todos los usuario + el editado
+      if (users.idUser == userToEdit.idUser) {
+        return (users = { ...userToEdit });
+      }
+      return users;
+    });
+
+    fs.writeFileSync(usersFilePath, JSON.stringify(newUser, null, " "));
+    res.redirect("/users/");
+  },
+
+  deleteUser: (req, res)=>{
+    let idUser = req.params.idUser;
+    let finalUsers = users.filter((user) => user.idUser != idUser);
+    fs.writeFileSync(
+      usersFilePath,
+      JSON.stringify(finalUsers, null, " ")
+    );
+    res.redirect("/usersList/"); //hacia una ruta
+  }
 };
 
 ////
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -7,9 +7,41 @@ const usersController = require("../controllers/usersControllers.js");
 //Router instance
 const router = express.Router();
 
-//Router methods
-router.get("/login", usersController.login);
-router.get("/register", usersController.register);
+//Require multer
+const multer = require("multer");
 
+//Require path
+const path = require("path");
+
+//Multer method
+var multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images/users");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "img-" + Date.now() + path.extname(file.originalname));
+  },
+});
+
+//Method upload
+var upload = multer({ storage: multerStorage });
+
+//Router methods
+router.get("/", usersController.index);
+//Detalla de un usuario
+router.get("/userDetail/:idUser", usersController.detail);
+
+router.get("/login", usersController.loginUser);
+//Views Create
+router.get("/register", usersController.register);
+//Create new user
+router.post("/", upload.single("userImage") ,usersController.registerUser);
+//Edit user
+router.get("/userEdit/:idUser", usersController.updateUser);
+//Update user
+router.put("/:idUser", usersController.updateUser);
+//Delete user
+router.delete("/delete/:idUser", usersController.deleteUser);
 ////
+
 module.exports = router;
