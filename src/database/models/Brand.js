454e739ecd49a4922486fe9ac52c6a91fe4e43a6
Rubens2214 <rubens_722@hotmail.es>
2022-11-12 15:56:57 -0300

    Correccion

--- src/database/models/Brand.js
+++ src/database/models/Brand.js
@@ -20,21 +20,14 @@ module.exports = (sequelize, dataTypes) => {
 
   const Brand = sequelize.define(alias, cols, config);
 
-<<<<<<< HEAD
+
   // Brand.associate = function (models) {
   //   Brand.hasMany(models.Product, {
   //     as: "products",
   //     foreingKey: "brand_id"
   //   })
   // }
-=======
-  Brand.associate = function (models) {
-    Brand.hasMany(models.Product, {
-      as: "products",
-      foreingKey: "brand_id",
-    });
-  };
->>>>>>> 971ce2513001922ba9c185bc026a21fd074a0f67
+
 
   return Brand;
 };
